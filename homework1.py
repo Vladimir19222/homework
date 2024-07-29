0f7d9d794c5ee33d23d94199840700ae4825a18e
Vladimir19222 <vi.karasev@mail.ru>
2024-07-29 17:49:12 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,16 @@
+example = 'Работа со строками'
+print(example[0])
+print(example[-1])
+print(example[9:17])
+print(example[::-1])
+print("Каждый второй символ строки: "+example[0:17:2])
+print(example[int(len(example )/2):-1])
+
+print(5%2)
+print(type(2))
+print(type('stol'))
+print(type(True))
+print(5>10)
+print(type(int('5')))
+
+
